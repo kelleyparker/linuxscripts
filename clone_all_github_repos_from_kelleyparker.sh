f4a031d6473faadc343e4f9dce355b800d83f0e7
Kelley Parker <kelleyparker@users.noreply.github.com>
2022-03-16 00:29:47 -0500

    Update clone_all_github_repos_from_kelleyparker.sh

--- clone_all_github_repos_from_kelleyparker.sh
+++ clone_all_github_repos_from_kelleyparker.sh
@@ -2,4 +2,4 @@
 
 # I got this script from https://stackoverflow.com/questions/19576742/how-to-clone-all-repos-at-once-from-github.  This allows me to clone all repositories from my Github to a single folder on Bash
 
-GHUSER=kelleyparkerr; curl "https://api.github.com/users/$GHUSER/repos?per_page=1000" | grep -o 'git@[^"]*' | xargs -L1 git clone
+GHUSER=kelleyparker; curl "https://api.github.com/users/$GHUSER/repos?per_page=1000" | grep -o 'git@[^"]*' | xargs -L1 git clone
